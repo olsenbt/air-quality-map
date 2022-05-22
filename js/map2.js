dfbc49e64582722e19dfd09dd6efd3a4d5de071c
olsenbt <bennettolsen@yahoo.com>
2022-05-21 20:02:45 -0700

    rename map2 data cols

--- js/map2.js
+++ js/map2.js
@@ -1,96 +1,108 @@
 // initialize basemmap
 mapboxgl.accessToken =
-'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+    'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
 const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/light-v10', // style URL
-zoom: 3, // starting zoom
-center: [-100, 40] // starting center
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    zoom: 3, // starting zoom
+    center: [-100, 40] // starting center
 });
+
+const grades = [10000, 20000, 100000],
+    colors = ['rgb(208,209,230)', 'rgb(120,120,0)', 'rgb(253,0,0)'],
+    radii = [5, 10, 15];
 
 // load data and add as layer
 async function geojsonFetch() {
-let response = await fetch('assets/map3/CRD2.geojson');
-let countyData = await response.json();
+    let response = await fetch('assets/map3/2011_GHG.geojson');
+    let countyData = await response.json();
 
-map.on('load', function loadingData() {
-    map.addSource('data', {
-        type: 'geojson',
-        data: countyData
+    map.on('load', function loadingData() {
+
+        map.addSource('data', {
+            type: 'geojson',
+            data: 'assets/map3/2011_GHG.geojson'
+        });
+
+        map.addLayer({
+                'id': 'emissionData-layer',
+                'type': 'circle',
+                'source': 'data',
+                'paint': {
+                    // increase the radii of the circle as the zoom level and dbh value increases
+                    'circle-radius': {
+                        'property': 'cases',
+                        'stops': [
+                            [{
+                                zoom: 5,
+                                value: grades[0]
+                            }, radii[0]],
+                            [{
+                                zoom: 5,
+                                value: grades[1]
+                            }, radii[1]],
+                            [{
+                                zoom: 5,
+                                value: grades[2]
+                            }, radii[2]]
+                        ]
+                    },
+                    'circle-color': {
+                        'property': 'cases',
+                        'stops': [
+                            [grades[0], colors[0]],
+                            [grades[1], colors[1]],
+                            [grades[2], colors[2]]
+                        ]
+                    },
+                    'circle-stroke-color': 'white',
+                    'circle-stroke-width': 1,
+                    'circle-opacity': 0.6
+                }
+            },
+            'waterway-label'
+        );
+
+        // click on tree to view magnitude in a popup
+        map.on('click', 'covid-point', (event) => {
+            new mapboxgl.Popup()
+                .setLngLat(event.features[0].geometry.coordinates)
+                .setHTML(`<strong>GHG Quantity:</strong> ${event.features[0].properties.cases}  (METRIC TONS CO2e)`)
+                .addTo(map);
+        });
+
+        // create legend
+        const legend = document.getElementById('legend');
+        legend.innerHTML = "<b>Emission by Facillity<br>(%)</b><br><br>";
+
+
+        layers.forEach((layer, i) => {
+            const color = colors[i];
+            const item = document.createElement('div');
+            const key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
+
+            const value = document.createElement('span');
+            value.innerHTML = `${layer}`;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+        });
     });
 
-    map.addLayer({
-        'id': 'countyData-layer',
-        'type': 'fill',
-        'source': 'data',
-        'paint': {
-            'fill-color': [
-                'step',
-                ['get', 'Pct_Change'],
-                '#FFEDA0',   // stop_output_0
-                18,          // stop_input_0
-                '#FEB24C',   // stop_output_1
-                51,          // stop_input_1
-                '#FC4E2A',   // stop_output_2
-                79,         // stop_input_2
-                '#E31A1C',   // stop_output_3
-                114,         // stop_input_3
-                '#BD0026',   // stop_output_4
-                225,        // stop_input_4
-                "#800026"    // stop_output_5
-            ],
-            'fill-outline-color': '#BBBBBB',
-            'fill-opacity': 0.7,
-        }
-    });
-
-    const layers = [
-        '< 17',
-        '18-50',
-        '51-78',
-        '79-114',
-        '114-224',
-        '225 and more'
-    ];
-    const colors = [
-        '#FFEDA070',
-        '#FEB24C70',
-        '#FC4E2A70',
-        '#E31A1C70',
-        '#BD002670',
-        '#80002670'
-    ];
-
-    // create legend
-    const legend = document.getElementById('legend');
-    legend.innerHTML = "<b>Mortality Rate Change<br>(%)</b><br><br>";
-
-
-    layers.forEach((layer, i) => {
-        const color = colors[i];
-        const item = document.createElement('div');
-        const key = document.createElement('span');
-        key.className = 'legend-key';
-        key.style.backgroundColor = color;
-
-        const value = document.createElement('span');
-        value.innerHTML = `${layer}`;
-        item.appendChild(key);
-        item.appendChild(value);
-        legend.appendChild(item);
-    });
-});
-
-map.on('mousemove', ({point}) => {
-    const state = map.queryRenderedFeatures(point, {
-        layers: ['countyData-layer']
-    });
-    document.getElementById('text-escription').innerHTML = state.length ?
-        `<h3>${state[0].properties.crd_Field1}</h3><p><strong><em>${state[0].properties.Pct_Change}</strong>% change</em></p>` :
-        `<p>Mortality rate % change is for 1985-2014.
+    map.on('mousemove', ({
+        point
+    }) => {
+        const state = map.queryRenderedFeatures(point, {
+            layers: ['emissionData-layer']
+        });
+        document.getElementById('text-escription').innerHTML = state.length ?
+            `<h3>${state[0].properties.crd_Field1}</h3><p><strong><em>${state[0].properties.Pct_Change}</strong>% change</em></p>` :
+            `<p>Mortality rate % change is for 1985-2014.
         Hover over a county!</p>
         <p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">The New York Times</a></p>`;
-});
+    });
 }
 
 geojsonFetch();
