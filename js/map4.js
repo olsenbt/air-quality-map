a1e02f6b8e1bc1661b556b50c28f3dc16a145ebc
tribiab <49213324+tribiab@users.noreply.github.com>
2022-06-07 02:37:01 -0700

    Update map4.js

--- js/map4.js
+++ js/map4.js
@@ -1,113 +1,101 @@
 // initialize basemmap
 mapboxgl.accessToken =
-   'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+    'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
 const map = new mapboxgl.Map({
-       container: 'map', // container ID
-       style: 'mapbox://styles/mapbox/light-v10', // style URL
-       zoom: 4, // starting zoom
-       center: [-100, 40], // starting center
-       //projection: 'albers'
-   });
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    zoom: 4, // starting zoom
+    center: [-100, 40], // starting center
+    projection: 'albers'
+});
 
-   // load data and add as layer
-   async function geojsonFetch() {
-       let response = await fetch('assets/map4/gge.geojson');
-       let countData = await response.json();
+// load data and add as layer
 
-       map.on('load', function loadingData() {
-           map.addSource('nses', {
-               type: 'geojson',
-               data: 'assets/map4/gge.geojson'
-           });
+async function geojsonFetch() {
+    let response = await fetch('assets/map4/myData2.geojson');
+    let tractData = await response.json();
 
-           map.addLayer({
-               'id': 'nses_value',
-               'type': 'fill',
-               'source': 'nses',
-               'paint': {
-                   'fill-color': [
-                       'step',
-                       ['get', 'nses_index'],
-                       '#fcf0bb', // stop_output_0
-                       38.0, // stop_input_0
-                       '#FED976', // stop_output_1
-                       58.0, // stop_input_1
-                       '#FEB24C', // stop_output_2
-                       76.0, // stop_input_2
-                       '#FD8D3C', // stop_output_3
-                       97.0, // stop_input_3
-                       '#fc4b26', // stop_output_4
-                       139, // stop_input_4
-                       '#800026', // stop_output_5
-                       291, // stop_input_5
-                       '#730022'
-                       /*,   // stop_output_6
-                                          193214,        // stop_input_6
-                                          "#800026",    // stop_output_7
-                                          756413,
-                                          "#730022"*/
-                   ],
-                   'fill-outline-color': '#BBBBBB',
-                   'fill-opacity': 0.7,
-               }
-           });
+    map.on('load', function loadingData() {
+        map.addSource('data', {
+            type: 'geojson',
+            data: tractData
+        });
 
-           const layers = [
-               '4 - 37',
-               '37 - 57',
-               '57 - 76',
-               '76 - 97',
-               '97 - 139',
-               '139 - 291'
-           ];
-           const colors = [
-               '#fcf0bb70',
-               '#FED97670',
-               '#FEB24C70',
-               '#FD8D3C70',
-               '#fc4b2670',
-               '#80002670',
-               "#73002270"
-               /*,
-                         '#BD002670',
-                         "#73002270"*/
-           ];
+        map.addLayer({
+            'id': 'nses_value',
+            'type': 'fill',
+            'source': 'data',
+            'paint': {
+                'fill-color': [
+                    'step',
+                    ['get', 'NSES'],
+                    '#fef0d9', // stop_output_0
+                    16.61, // stop_input_0
+                    '#fde4c3', // stop_output_1
+                    38.3, // stop_input_1
+                    '#fdbb84', // stop_output_2
+                    48.45, // stop_input_2
+                    '#fc8d59', // stop_output_3
+                    58.12, // stop_input_3
+                    '#e34a33', // stop_output_4
+                    69.6, // stop_input_4
+                    "#b30000", // stop_output_5
+                    91.7,
+                    "#b30000"
+                ],
+                'fill-opacity': .9
+            }
+        });
 
-           // create legend
-           const legend = document.getElementById('legend');
-           legend.innerHTML = "<b>Socioeconomic Status Index Value </b><br><br>";
+        const layers = [
+            '<16',
+            '16 - 38',
+            '38 - 48',
+            '48 - 58',
+            '58 - 69',
+            '69 - 92 HIGH SES'
+        ];
+        // #273859|#4a5d83|#6d81ac|#8fa5d6|#b2caff
+        const colors = [
+            '#fef0d9', '#fde4c3', '#fdbb84', '#fc8d59', '#e34a33', '#b30000'
+        ];
+
+        // create legend
+        const legend = document.getElementById('legend');
+        legend.innerHTML = "<b>Socioeconomic Status Score </b><br><br>";
 
 
-           layers.forEach((layer, i) => {
-               const color = colors[i];
-               const item = document.createElement('div');
-               const key = document.createElement('span');
-               key.className = 'legend-key';
-               key.style.backgroundColor = color;
+        layers.forEach((layer, i) => {
+            const color = colors[i];
+            const item = document.createElement('div');
+            const key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
 
-               const value = document.createElement('span');
-               value.innerHTML = `${layer}`;
-               item.appendChild(key);
-               item.appendChild(value);
-               legend.appendChild(item);
-           });
-       });
+            const value = document.createElement('span');
+            value.innerHTML = `${layer}`;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+        });
+    });
 
-       map.on('mousemove', ({
-           point
-       }) => {
-           const state = map.queryRenderedFeatures(point, {
-               layers: ['nses_value']
-           });
-           document.getElementById('text-escription').innerHTML = state.length ?
-               `<h3>${state[0].properties.county}, ${state[0].properties.state}:</h3><p><strong><em>${state[0].properties.nses_index}</strong>% Index value.</em></p>` :
-               `<p>Hover over a county!</p>`;
-       });
-   }
+    map.on('mousemove', ({
+        point
+    }) => {
+        const state = map.queryRenderedFeatures(point, {
+            layers: ['nses_value']
+        });
+        document.getElementById('text-escription').innerHTML = state.length ?
+            `<h3>${state[0].properties.COUNTY} County, Census Tract ${state[0].properties.ses_TRACT}</h3><p><strong><em>${state[0].properties.NSES}</strong> SES Score.</em></p>` :
+            `<p>Hover over a county!</p>`;
+    });
+}
 
-   geojsonFetch();
-   // add the data source
-   const source =
-       '<p style="text-align: right; font-size:10pt">Source: <a href="https://www.arcgis.com/home/item.html?id=2a98d90305364e71866443af2c9b5d06">USGS</a></p>';
-   // combine all the html codes.
-   legend.innerHTML = labels.join('') + source;
+
+geojsonFetch();
+// add the data source
+const source =
+'<p style="text-align: right; font-size:10pt">Source: <a href="https://www.arcgis.com/home/item.html?id=2a98d90305364e71866443af2c9b5d06">USGS</a></p>';
+// combine all the html codes.
+legend.innerHTML = labels.join('') + source;
